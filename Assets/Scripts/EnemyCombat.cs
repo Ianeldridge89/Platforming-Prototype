ab3bf24ec87c83c6f5c56f7bf8ef8f5bdc6af8d2
ianeldridge89@gmail.com <Ianeldridge89@gmail.com>
2022-04-03 23:15:28 +0100

    0.2.3.1
    - added raycast line of sight for enemy

--- Assets/Scripts/EnemyCombat.cs
+++ Assets/Scripts/EnemyCombat.cs
@@ -9,12 +9,46 @@ public class EnemyCombat : MonoBehaviour
     private BoxCollider2D enemyCollider;
     private Rigidbody2D enemyBody;
     static public int enemyDamage;
+    private Transform sightStart;
+    private Transform sightEnd;
 
     private void Start()
     {
         enemyCollider = GetComponent<BoxCollider2D>();
         enemyBody = GetComponent<Rigidbody2D>();
         enemyDamage = 10;
+    }
+
+    void FixedUpdate()
+    {
+
+
+        /*Vector2 forward = transform.TransformDirection(Vector2.left) * 5;
+        Debug.DrawRay(transform.position, forward, Color.green);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, (forward * 5));
+
+        if (hit.collider != null)
+        {
+            Debug.Log("PLAYER SEEN!");
+        }
+        else
+        {
+            Debug.Log("PLAYER NOT SEEN");
+        }*/
+    }
+
+
+
+
+
+    private void ScanRight()
+    {
+        
+    }
+
+    private void ScanLeft()
+    {
+
     }
 
     // if the player enters the enemy's field of view,
--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -62,10 +62,12 @@ public class EnemyMovement : MonoBehaviour
     private void MoveLeft()
     {
         transform.Translate(Vector2.left * movementSpeed * Time.deltaTime);
+        Debug.DrawRay(transform.position, Vector2.left * 6, Color.green);
     }
 
     private void MoveRight()
     {
         transform.Translate(Vector2.right * movementSpeed * Time.deltaTime);
+        Debug.DrawRay(transform.position, Vector2.right * 6, Color.green);
     }
 }
