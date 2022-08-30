a2d5a272d526f3b627b849082390125f662133cc
ianeldridge89@gmail.com <Ianeldridge89@gmail.com>
2022-08-30 23:21:25 +0100

    2.5.6.1

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,6 +4,45 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public GameObject player;
+    public float positionZ;
+
+    private void Start()
+    {
+        positionZ = transform.position.z;
+    }
+
+    void Update()
+    {
+
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, positionZ);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+    //current options
     public Transform target;
     public float smoothSpeed = 0.125f;
     public float offsetX;
@@ -13,19 +52,33 @@ public class CameraController : MonoBehaviour
 
     public void Start()
     {
+        //current options
         offsetX = 0;
         offsetY = 5;
         offsetZ = -50;
         offset = new Vector3(offsetX, offsetY, offsetZ);
+        //new options
+
     }
 
 
     private void FixedUpdate()
+    {
+        
+    }
+
+    public void newCameraMethod()
+    {
+
+    }
+
+    public void currentCameraMethod()
     {
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
         transform.LookAt(target);
     }
+    */
 
 }
